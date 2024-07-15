014d5b514d6646e5e8200f942fea23e3166d53d7
Catherine Cabrera <catheria12@gmail.com>
2024-07-15 15:07:31 -0400

    fix background styling

--- src/App.tsx
+++ src/App.tsx
@@ -9,14 +9,14 @@ export const App = () => {
   const [imgUrl, setImgUrl] = useState(initialUrl);
 
   return (
-    <>
+    <div className="min-h-screen bg-light-yellow">
       <Nav />
-      <div className="grid place-items-center py-4 gap-4 bg-light-yellow">
+      <div className="grid place-items-center py-4 gap-4 h-full">
         <p className="bg-white border-cyan-500 font-mono border-4 p-2 rounded-lg mx-1 text-off-blue font-medium hover:bg-slate-50">
           Time Elapsed: {formatTime(useGame().seconds)}
         </p>
         <Board imgUrl={imgUrl} onChangeImg={setImgUrl} />
       </div>
-    </>
+    </div>
   );
 };
