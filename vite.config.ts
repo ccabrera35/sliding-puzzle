1ef3543f4babc5ba4eb6e450c5edb963f969dafc
Catherine Cabrera <catheria12@gmail.com>
2024-07-23 02:47:09 -0400

    add base path

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const isProd = process.env.NODE_ENV === "production";
+
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: isProd ? "/sliding-puzzle" : "",
   plugins: [react()],
 })
