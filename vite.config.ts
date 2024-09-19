eb6b10e83291d3071e7e27e8d5f86a77d4a95dcc
Catherine Cabrera <catheria12@gmail.com>
2024-09-19 00:41:38 -0400

    fix: remove basepath in config

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-const isProd = process.env.NODE_ENV === "production";
-
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: isProd ? "/sliding-puzzle" : "",
   plugins: [react()],
 })
